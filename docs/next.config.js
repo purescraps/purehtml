f4af754740533a97926978c7d632f427e042a376
Fatih Kaya <1994274@gmail.com>
2024-05-17 13:58:46 +0300

    docs: added github workflow

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,10 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   output: 'export',
-  distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/purehtml' : '',
   poweredByHeader: false,
   reactStrictMode: true,
   assetPrefix: './',
