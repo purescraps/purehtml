163d2bb2c1d9d8efc6117da40f40f1b99115addb
Fatih Kaya <1994274@gmail.com>
2024-05-17 12:05:06 +0300

    docs: fix asset prefix

--- docs/next.config.js
+++ docs/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'dist',
+  basePath: process.env.NODE_ENV === 'production' ? '/purehtml' : '',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  assetPrefix: './',
 };
 
 export default nextConfig;
