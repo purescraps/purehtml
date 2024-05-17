81b39d9c08369a73ac926e9a44152f6cecc33d29
Fatih Kaya <1994274@gmail.com>
2024-05-17 11:51:10 +0300

    Updates

---
+++ _next/static/chunks/pages/_error-4ecd48555d48c733.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{83083:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(69778)}])}},function(n){n.O(0,[888,928,179],function(){return n(n.s=83083)}),_N_E=n.O()}]);
