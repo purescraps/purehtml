07ca18d43568bf4184879867a1ee8855ce2d1589
onurc <e224345@metu.edu.tr>
2024-11-26 20:02:21 +0300

    Classes moved to src/main/java folder for maven integrity. Pom.xml file updated accordingly to work with Specs.

--- java/com/purescraps/purehtml/configs/Config.java
+++
@@ -1,8 +0,0 @@
-package com.purescraps.purehtml.configs;
-
-import com.purescraps.purehtml.interfaces.ExtractParams;
-
-public abstract class Config {
-        public abstract Object extract(ExtractParams params);
-    }
-
---
+++ java/src/main/java/org/purescraps/purehtml/transformers/TransformParams.java
@@ -0,0 +1,38 @@
+package org.purescraps.purehtml.transformers;
+
+import org.jsoup.nodes.Element;
+
+public class TransformParams {
+
+    private Object val;
+    private Element element;
+    private final String url;
+
+    // Constructor
+    public TransformParams(Object val, Element element, String url) {
+        this.val = val;
+        this.element = element;
+        this.url = url;
+    }
+    // Getters and Setters
+    public Object getVal() {
+        return val;
+    }
+
+    public void setVal(Object val) {
+        this.val = val;
+    }
+
+    public Element getElement() {
+        return element;
+    }
+
+    public void setElement(Element $el) {
+        this.element = $el;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
