6f95c7a466dc4a2fdd35f3309c6d9ac7c886c44c
Fatih Kaya <1994274@gmail.com>
2026-09-11 00:12:23 +0300

    next config ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,7 +1,6 @@
-// @ts-check
+import { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'export',
   poweredByHeader: false,
   reactStrictMode: true,
